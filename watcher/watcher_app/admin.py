75e1b232d9d4dd9d1bba6b6554ec209f616eeed6
Nil Şen <nil.sen@ogr.gsu.edu.tr>
2025-03-03 13:48:38 +0300

    Basic sign up login page

--- watcher/watcher_app/admin.py
+++ watcher/watcher_app/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import User, Genre, Movie, Review, CommentInteraction, FavoriteMovie, Follow
 
-admin.site.register(User)
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(Review)
-admin.site.register(CommentInteraction)
-admin.site.register(FavoriteMovie)
-admin.site.register(Follow)
+# Register your models here.
