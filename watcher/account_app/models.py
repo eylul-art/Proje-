dae5a92195ac035e3f6126310565027ffbf9b401
Nil Şen <nil.sen@ogr.gsu.edu.tr>
2025-03-05 09:07:52 +0300

    Mail sistemi ve şifremi unuttum özelliği basit

--- watcher/account_app/models.py
+++ watcher/account_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Admin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_superuser = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
--- watcher/watcher/settings.py
+++ watcher/watcher/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',  # Required by allauth
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',  # If you want to enable social login
+    #'allauth.socialaccount',  # If you want to enable social login
 ]
 
 SITE_ID = 1
@@ -52,14 +52,20 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Allauth settings
-ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_LOGIN_METHODS = {'username', 'email'}
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Add email backend for sending verification emails
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'nil.sen0721@gmail.com'
+EMAIL_HOST_PASSWORD = 'pksm jpxa fbkw ejtq'
+DEFAULT_FROM_EMAIL = 'nil.sen0721@gmail.com'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
